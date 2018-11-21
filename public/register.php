62759d16d41bb7f942a035e19675db36da44ac1a
alezvi <alezvi@users.noreply.github.com>
2018-11-21 10:32:01 -0300

    Create register.php

---
+++ public/register.php
@@ -0,0 +1,13 @@
+<form method="post">
+    <div>
+        <input type="text" placeholder="Ingrese su email" name="email">
+    </div>
+
+    <div>
+        <input type="password" placeholder="Ingrese su clave" name="password">
+    </div>
+
+    <div>
+        <button>Enviar</button>
+    </div>
+</form>
